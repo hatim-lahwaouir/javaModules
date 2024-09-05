e9bbfe39507ebb2ec0ee518bd188d0c84461ac53
Hatim Lahwaouir <hlahwaou@e3r10p10.1337.ma>
2024-09-05 16:32:40 +0100

    ex04

--- Module00/ex04/Program.java
+++ Module00/ex04/Program.java
@@ -23,14 +23,15 @@ public class Program{
         }
 
 
-    static  int [] countCharacters(String s){
+    static  int [] [] countCharacters(String s){
         int len = s.length() - 2;
 
         int max = 0;
         int min = 999;
 
-        int [] arr = new int [28];
+        int [] arr = new int [26];
 
+        int [][] res = new int [11][26];
 
         for (int i = 0; i < len;i++){
             char c = s.charAt(i);
@@ -40,14 +41,35 @@ public class Program{
                 max =  arr[c - 'A'];
             if (min > arr[c - 'A'])
                 min =  arr[c - 'A'];
-
         }
+    
+        System.out.printf("%d %d\n", max, min);
         for (int i = 0; i < 26; i++){
-            System.out.printf("%d ", arr[i]);
+            if (arr[i] == 0)
+                continue;
+
+            float prop = ((float)arr[i] * 100 ) / (float)max;
+            prop /= 10;
+        
+            res [(int)prop][i] = 1;
+            // System.out.printf("%f ", prop);
         }
 
-        return arr;
-    } 
+        for (int i = 1; i < 11; i++){
+            for (int j = 0; j < 26; j++){
+                if (res[i][j] != 0)
+                    System.out.printf("# ");
+            }
+            System.out.print('\n');
+        }
+
+
+        
+
+        return res;
+    }
+    
+    
     public static void main(String[] args) {
         String inp;
 
